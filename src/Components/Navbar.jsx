16d56dcaccf8347b1c44b3eaee34a7469c3bfaf1
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-05-11 08:16:32 -0600

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ const NavBar = () => {
     <nav className="Navbar mb-5">
       <div className="container-fluid">
         <h3 className="mx-auto">React Weather App</h3>
+        <img src="/logo192.png" className="logo react" alt="React logo" />
       </div>
     </nav>
   );

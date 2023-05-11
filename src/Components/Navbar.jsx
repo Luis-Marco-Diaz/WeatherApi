4603cc410ae81343b7ad554d4b4aad19a2c1cac6
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-05-11 08:40:03 -0600

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar mb-5">
       <div className="container-fluid">
         <h3 className="mx-auto">React Weather App</h3>
         <img src="/logo192.png" className="logo react" alt="React logo" />
